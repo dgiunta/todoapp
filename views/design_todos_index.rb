a118a97efdb8d2861cd2307e9caca3b5a0ab54d8
Matt Puchlerz <matt@puchlerz.com>
2010-06-25 09:29:57 -0500

    More stylin. Section headers are getting close.

--- views/design_todos_index.rb
+++ views/design_todos_index.rb
@@ -4,18 +4,23 @@ module WhatsNext
       
       def todos
         todos = [
-          { :title => 'Take out the trash',                             :checked_attribute => 'checked' },
-          { :title => 'Do the dishes',                                  :checked_attribute => 'checked' },
-          { :title => 'Clean the kat litter',                           :checked_attribute => 'checked' },
-          { :title => 'Find a lightning talk presenter',                :checked_attribute => '' },
-          { :title => 'Find a main presenter',                          :checked_attribute => '' },
-          { :title => 'Get a Refresh Chicago DBA',                      :checked_attribute => '' },
-          { :title => 'Create and submit the Refresh expense report',   :checked_attribute => '' },
-          { :title => 'Send Jon&rsquo;s parents a thank you',           :checked_attribute => '' },
-          { :title => 'Get groceries',                                  :checked_attribute => '' },
+          { :title => 'Take out the trash' },
+          { :title => 'Do the dishes' },
+          { :title => 'Clean the kat litter' },
+          { :title => 'Find a lightning talk presenter' },
+          { :title => 'Find a main presenter' },
+          { :title => 'Get a Refresh Chicago DBA' },
+          { :title => 'Create and submit the Refresh Chicago expense report with receipts' },
+          { :title => 'Send Jon&rsquo;s parents a thank you' },
+          { :title => 'Get groceries' },
         ]
         
-        todos.each_with_index { |t, i| todos[i][:id] = i }
+        todos += Marshal.load( Marshal.dump(todos) )
+        
+        todos.each_with_index do |t, i| 
+          todos[i][:id] = i
+          todos[i][:checked_attribute] = 'checked' if i < 4
+        end
       end
       
     end
