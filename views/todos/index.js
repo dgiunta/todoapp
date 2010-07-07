a3d80bf7b76f91ec64df296b43039bffaf8334f4
Matt Puchlerz <matt@puchlerz.com>
2010-07-06 22:19:35 -0500

    Refactored the Mustache views to be JS objects.

--- views/todos/index.js
+++ views/todos/index.js
@@ -1,48 +1,44 @@
-module WhatsNext
-  module Views
-    class DesignTodosIndex < Layout
-      
-      def todos
-        todos = [
-          { :title => 'Take out the trash' },
-          { :title => 'Do the dishes' },
-          { :title => 'Clean the cat litter' },
-          { :title => 'Find a lightning talk presenter' },
-          { :title => 'Find a main presenter' },
-          { :title => 'Get a Refresh Chicago DBA' },
-          { :title => 'Create and submit the Refresh Chicago expense report with receipts' },
-          { :title => 'Send Jon&rsquo;s parents a thank you' },
-          { :title => 'Get groceries' },
-          { :title => 'Take out the trash' },
-          { :title => 'Do the dishes' },
-          { :title => 'Clean the cat litter' },
-          { :title => 'Find a lightning talk presenter' },
-          { :title => 'Find a main presenter' },
-          { :title => 'Get a Refresh Chicago DBA' },
-          { :title => 'Create and submit the Refresh Chicago expense report with receipts' },
-          { :title => 'Send Jon&rsquo;s parents a thank you' },
-          { :title => 'Get groceries' },
-          { :title => 'Take out the trash' },
-          { :title => 'Do the dishes' },
-          { :title => 'Clean the cat litter' },
-          { :title => 'Find a lightning talk presenter' },
-          { :title => 'Find a main presenter' },
-          { :title => 'Get a Refresh Chicago DBA' },
-          { :title => 'Create and submit the Refresh Chicago expense report with receipts' },
-          { :title => 'Send Jon&rsquo;s parents a thank you' },
-          { :title => 'Get groceries' },
-        ]
-        
-        todos.each_with_index do |t, i| 
-          todos[i][:id] = i
-          todos[i][:checked_attribute] = 'checked' if i < 4
-        end
-        
-        todos.insert 7, { :separator => true }
-        
-        todos
-      end
-      
-    end
-  end
-end
+WhatsNext.Mustache.Views['todos/index'] = {      
+  
+  todos: function() {
+    var todos = [
+      { title: 'Take out the trash' },
+      { title: 'Do the dishes' },
+      { title: 'Clean the cat litter' },
+      { title: 'Find a lightning talk presenter' },
+      { title: 'Find a main presenter' },
+      { title: 'Get a Refresh Chicago DBA' },
+      { title: 'Create and submit the Refresh Chicago expense report with receipts' },
+      { title: 'Send Jon&rsquo;s parents a thank you' },
+      { title: 'Get groceries' },
+      { title: 'Take out the trash' },
+      { title: 'Do the dishes' },
+      { title: 'Clean the cat litter' },
+      { title: 'Find a lightning talk presenter' },
+      { title: 'Find a main presenter' },
+      { title: 'Get a Refresh Chicago DBA' },
+      { title: 'Create and submit the Refresh Chicago expense report with receipts' },
+      { title: 'Send Jon&rsquo;s parents a thank you' },
+      { title: 'Get groceries' },
+      { title: 'Take out the trash' },
+      { title: 'Do the dishes' },
+      { title: 'Clean the cat litter' },
+      { title: 'Find a lightning talk presenter' },
+      { title: 'Find a main presenter' },
+      { title: 'Get a Refresh Chicago DBA' },
+      { title: 'Create and submit the Refresh Chicago expense report with receipts' },
+      { title: 'Send Jon&rsquo;s parents a thank you' },
+      { title: 'Get groceries' }
+    ];
+    
+    todos.each( function(todo, i) { 
+      todo.id = i;
+      if (i < 4) todo.checked_attribute = 'checked';
+    });
+    
+    // todos.insert 7, { :separator => true }
+    
+    return todos;
+  }
+  
+};
--- views/todos/show.js
+++ views/todos/show.js
@@ -1,23 +1,21 @@
-module WhatsNext
-  module Views
-    class DesignTodosShow < Layout
-      
-      def tags
-        tags = [
-          { :title => 'Errand', :checked_attribute => 'checked' },
-          { :title => 'Errand: Groceries' },
-          { :title => 'Home' },
-          { :title => 'Refresh Chicago' },
-          { :title => 'Refresh Chicago: May 2010' },
-          { :title => 'Refresh Chicago: June 2010', :checked_attribute => 'checked' },
-          { :title => 'Work' },
-        ]
-        
-        tags.each_with_index do |t, i| 
-          tags[i][:id] = i
-        end
-      end
-      
-    end
-  end
-end
+WhatsNext.Mustache.Views['todos/show'] = {      
+  
+  tags: function() {
+    var tags = [
+      { title: 'Errand', checked_attribute: 'checked' },
+      { title: 'Errand: Groceries' },
+      { title: 'Home' },
+      { title: 'Refresh Chicago' },
+      { title: 'Refresh Chicago: May 2010' },
+      { title: 'Refresh Chicago: June 2010', checked_attribute: 'checked' },
+      { title: 'Work' }
+    ];
+    
+    tags.each( function(tag, i) {
+      tags.id = i;
+    });
+    
+    return tags;
+  }
+  
+};
