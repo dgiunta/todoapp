d1a02f6d6384e31beb2e7d430213cb1145173bc5
Matt Puchlerz <matt@puchlerz.com>
2010-07-07 19:36:47 -0500

    Got the index page rendering through Mustache JS.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,3 +4,41 @@ WhatsNext.Mustache = {
   Templates: {},
   Views: {}
 };
+
+WhatsNext.Page = new Class({
+  
+  element: null,
+  path: null,
+  
+  initialize: function(path) {
+    this.path = path;
+  },
+  
+  destroy: function() {
+    this.element.dispose();
+  },
+  
+  render: function() {
+    var rendered_template = Mustache.to_html( 
+      WhatsNext.Mustache.Templates[ this.path + '.html' ], 
+      WhatsNext.Mustache.Views[ this.path ] 
+    );
+
+    this.element = new Element('div', { html: rendered_template }).getFirst();
+    document.body.adopt(this.element);
+    
+    return this.element;
+  }
+  
+});
+
+
+
+window.addEvent('domready', function() {
+  
+  window.todosIndex = new WhatsNext.Page('todos/index');
+  window.todosShow  = new WhatsNext.Page('todos/show');
+  
+  window.todosIndex.render();
+  
+});
