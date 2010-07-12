91ecdcbb3da7b71f50d6810fa922aa0c46f45548
Matt Puchlerz <matt@puchlerz.com>
2010-07-12 09:10:18 -0500

    Added todos/new markup

--- views/todos/new.js
+++ views/todos/new.js
@@ -1,5 +1,23 @@
 WhatsNext.Mustache.Views['todos/new'] = {      
   
+  tags: function() {
+    var tags = [
+      { title: 'Errand' },
+      { title: 'Errand: Groceries' },
+      { title: 'Home' },
+      { title: 'Refresh Chicago' },
+      { title: 'Refresh Chicago: May 2010' },
+      { title: 'Refresh Chicago: June 2010' },
+      { title: 'Work' }
+    ];
+    
+    tags.each( function(tag, i) {
+      tags.id = i;
+    });
+    
+    return tags;
+  },
+
   title: 'New'
     
 };
