34f6630fc83e853d387a8b4682aecb13691b5161
Matt Puchlerz <matt@puchlerz.com>
2010-07-28 09:01:30 -0500

    Reorganized

--- public/javascripts/whats_next/base.js
+++ public/javascripts/whats_next/base.js
@@ -6,8 +6,31 @@ WhatsNext = {
 
 (function(_) {
   
+  
+  
   _.log = function(message) {
     if (_.logging) console.log(message);
+  };
+
+  _.redirect = function(path) {
+    _.log('REDIRECT "' + path + '"');
+    window.location.hash = '#' + path;
+  };
+
+  _.callRouteFromHash = function() {
+    var path   = window.location.hash.substr(1);
+    var routes = _.routes.getKeys();
+  
+    for (var i = 0; i < routes.length; i++) {
+      var match = path.match( routes[i] );
+      if (match) {
+        _.log('ROUTE "' + path + '"  -->  "' + routes[i] + '"');
+        _.routes[ routes[i] ].apply( null, match.splice(1) );
+        return;
+      }
+    };
+  
+    _.redirect('/');
   };
 
 
@@ -162,26 +185,5 @@ WhatsNext = {
   });
 
 
-
-  _.redirect = function(path) {
-    _.log('REDIRECT "' + path + '"');
-    window.location.hash = '#' + path;
-  };
-
-  _.callRouteFromHash = function() {
-    var path   = window.location.hash.substr(1);
-    var routes = _.routes.getKeys();
-  
-    for (var i = 0; i < routes.length; i++) {
-      var match = path.match( routes[i] );
-      if (match) {
-        _.log('ROUTE "' + path + '"  -->  "' + routes[i] + '"');
-        _.routes[ routes[i] ].apply( null, match.splice(1) );
-        return;
-      }
-    };
-  
-    _.redirect('/');
-  };
 
 })(WhatsNext);
