6a498a8623ecf2a337ba16480f6496e0dbd73819
Matt Puchlerz <matt@puchlerz.com>
2010-07-30 07:38:56 -0500

    Describing logging

--- spec/javascripts/base_spec.js
+++ spec/javascripts/base_spec.js
@@ -1,8 +1,33 @@
 describe('Base', function() {
   
   it('should have a common namespace', function() {
-    expect(WhatsNext).not.toBeNull();
+    expect(WhatsNext).not.toBe(null);
   });
+  
+  describe('logging', function() {
+    
+    it('has logging on by default', function() {
+      expect(WhatsNext.logging).toBe(true);
+    });
+    
+    it('logs to the console', function() {
+      spyOn(console, 'log');
+      WhatsNext.log('Hello!');
+      expect(console.log).toHaveBeenCalledWith('Hello!');
+    });
+    
+    it('does NOT log when logging has been disabled', function() {
+      var original = WhatsNext.logging;
+      WhatsNext.logging = false;
+      
+      spyOn(console, 'log');
+      WhatsNext.log('Hello!');
+      expect(console.log).not.toHaveBeenCalled();
+      
+      WhatsNext.logging = original;
+    });
 
+  });
+  
 });
 
