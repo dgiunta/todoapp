89be0421739b678b0df336dae7b5224b41ac5201
Matt Puchlerz <matt@puchlerz.com>
2010-07-30 09:23:29 -0500

    Added specs for routing requests. Last one is not passing, but it is a test problem

--- spec/javascripts/base_spec.js
+++ spec/javascripts/base_spec.js
@@ -1,4 +1,4 @@
-describe('Base', function() {
+describe('WhatsNext base functionality', function() {
   
   it('should have a common namespace', function() {
     expect(WhatsNext).not.toBe(null);
@@ -35,6 +35,50 @@ describe('Base', function() {
       window.location.hash = '#/';
       WhatsNext.redirect('/path/to/something');
       expect(window.location.hash).toEqual('#/path/to/something');
+    });
+    
+  });
+  
+  describe('routing requests', function() {
+    
+    beforeEach( function() {
+      originalRoutes = WhatsNext.routes;
+      WhatsNext.routes = new Hash({
+        '^/$': $empty,
+        '^/posts$': $empty,
+        '^/posts/(\\d+)/(\\w+)$': $empty
+      });
+    });
+    
+    afterEach( function() {
+      WhatsNext.routes = originalRoutes;
+    });
+    
+    it('finds the appropriate regex route from the hash and calls it', function() {
+      var route = '^/$';
+      spyOn(WhatsNext.routes, route);
+      window.location.hash = '#/';
+      
+      WhatsNext.callRouteFromHash();
+      expect( WhatsNext.routes[route] ).toHaveBeenCalled();
+    });
+    
+    it('finds the another regex route from the hash and calls it', function() {
+      var route = '^/posts$';
+      spyOn(WhatsNext.routes, route);
+      window.location.hash = '#/posts';
+      
+      WhatsNext.callRouteFromHash();
+      expect( WhatsNext.routes[route] ).toHaveBeenCalled();
+    });
+    
+    it('passes any matched groups in the regex to the called function', function() {
+      var route = '^/posts/(\\d+)/(\\w+)$';
+      spyOn(WhatsNext.routes, route);
+      window.location.hash = '#/posts/45/edit';
+      
+      WhatsNext.callRouteFromHash();
+      expect( WhatsNext.routes[route] ).toHaveBeenCalledWith([ '45', 'edit' ]);
     });
     
   });
