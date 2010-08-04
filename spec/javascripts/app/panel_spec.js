f0c8279c1f47aed788461f68bc1cb7594aedbb99
Matt Puchlerz <matt@puchlerz.com>
2010-08-04 08:52:28 -0500

    Cleaned up the spec

--- spec/javascripts/app/panel_spec.js
+++ spec/javascripts/app/panel_spec.js
@@ -12,7 +12,9 @@ describe('WhatsNext.Panel', function() {
     originalViews = WhatsNext.Views;
     WhatsNext.Views = $H({
       '/path': new Class()
-    });      
+    });  
+    
+    panel = new WhatsNext.Panel('/path');    
   });
   
   afterEach( function() {
@@ -27,15 +29,12 @@ describe('WhatsNext.Panel', function() {
   });
   
   it('does NOT let you initialize with an already used path', function() {
-    new WhatsNext.Panel('/path');
-    
     expect( function() {
       new WhatsNext.Panel('/path'); 
     }).toThrow('Error: already initialized panel with path "/path"');
   });
   
   it('keeps track of a containing element', function() {
-    var panel = new WhatsNext.Panel('/path');
     expect( panel.element ).toBe(null);
   });
   
@@ -43,8 +42,6 @@ describe('WhatsNext.Panel', function() {
   
     it('renders the template with Mustache', function() {
       spyOn(Mustache, 'to_html');
-  
-      var panel = new WhatsNext.Panel('/path');
       panel.renderTemplate();
   
       expect(Mustache.to_html).toHaveBeenCalledWith( 
@@ -56,14 +53,14 @@ describe('WhatsNext.Panel', function() {
     it('throws an error if the associated view class is NOT present', function() {
       WhatsNext.Views = {};
       expect( function() {
-        new WhatsNext.Panel('/path').renderTemplate();
+        panel.renderTemplate();
       }).toThrow('Error: could not find the view at "/path"');
     });
 
     it('throws an error if the associated template is NOT present', function() {
       WhatsNext.Templates = {};
       expect( function() {
-        new WhatsNext.Panel('/path').renderTemplate();
+        panel.renderTemplate();
       }).toThrow('Error: could not find the template at "/path.html"');
     });
   
@@ -74,9 +71,7 @@ describe('WhatsNext.Panel', function() {
     describe('when the containing element does NOT yet exist', function() {
   
       it('creates the containing element using the rendered template', function() {
-        var panel = new WhatsNext.Panel('/path');
         spyOn(panel, 'renderTemplate').andReturn('<section>Where you at dawg?</section>');
-        
         panel.render();
         
         expect( panel.renderTemplate ).toHaveBeenCalled();
@@ -84,7 +79,6 @@ describe('WhatsNext.Panel', function() {
       });
   
       it('injects the containing element into the document body', function() {
-        var panel = new WhatsNext.Panel('/path');
         panel.render();
         expect( panel.element.getParent() ).toBe(document.body);
       });
@@ -92,6 +86,10 @@ describe('WhatsNext.Panel', function() {
     });
   
     describe('when the containing element exists', function() {
+    
+      beforeEach( function() {
+        
+      });
     
     });
   
