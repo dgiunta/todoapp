62428d81292ce935d81bcc297e9798ab28b70913
Matt Puchlerz <matt@puchlerz.com>
2010-08-06 21:57:04 -0500

    Speced out the "/" and "/todos" routes.

--- spec/javascripts/app/routes_spec.js
+++ spec/javascripts/app/routes_spec.js
@@ -1,9 +1,41 @@
 describe('WhatsNext.Routes', function() {
   
-  it('redirects "/" to "/todos"', function() {
-    spyOn(WhatsNext, 'redirect');
-    WhatsNext.route('/');
-    expect(WhatsNext.redirect).toHaveBeenCalledWith('/todos');
+  beforeEach( function() {
+    originalPanels = WhatsNext.Panel._panels;
+    WhatsNext.Panel._panels = [];
+  });
+  
+  afterEach( function() {
+    WhatsNext.Panel._panels = originalPanels;
+  });
+  
+  describe('/', function() {
+    
+    it('redirects to /todos', function() {
+      spyOn(WhatsNext, 'redirect');
+      WhatsNext.route('/');
+      expect(WhatsNext.redirect).toHaveBeenCalledWith('/todos');
+    });
+
+  });
+  
+  describe('/todos', function() {
+    
+    it('creates a panel', function() {
+      spyOn(WhatsNext.Panel, 'findOrCreate').andCallThrough();
+      WhatsNext.route('/todos');
+      expect(WhatsNext.Panel.findOrCreate).toHaveBeenCalledWith('/todos/index');
+    });
+    
+    it('renders the panel', function() {
+      thePanel = new WhatsNext.Panel();
+      spyOn(WhatsNext.Panel, 'findOrCreate').andReturn(thePanel);
+      spyOn(thePanel, 'render');
+      
+      WhatsNext.route('/todos');
+      expect(thePanel.render).toHaveBeenCalled();
+    });
+    
   });
   
 });
